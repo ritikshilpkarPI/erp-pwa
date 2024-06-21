a88e28bb273a354b45c0bf45252a3b872b3e1dbe
Rishabh Pillay <rishabhpillay03@gmail.com>
2024-06-21 15:49:39 +0530

    create ticket page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Ticket from './components/ticket/Ticket';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
        <h1>ERP PWA APP</h1>
-      </header>
+      </header> */}
+      <Ticket/>
+
     </div>
   );
 }
