486cfdcd66c28c9c51beebd632d23612fd42bdd0
akshu2626 <akshugajbhiye97@gmail.com>
2024-06-27 12:30:12 +0530

    Integrate Signup page Api

--- src/components/signupPage/SignupPage.jsx
+++ src/components/signupPage/SignupPage.jsx
@@ -1,86 +1,102 @@
-import React, { useState } from 'react'
-import './SignupPage.css';
+import React, { useState } from "react";
+import "./SignupPage.css";
 
-import NavigationHeader from '../navigationHeader/NavigationHeader';
-import backButtonImage from '../../image/BackButton.svg'
-import ButtonInput from '../buttonInput/ButtonInput';
-import TextInput from '../textInput/TextInput';
+import NavigationHeader from "../navigationHeader/NavigationHeader";
+import backButtonImage from "../../image/BackButton.svg";
+import ButtonInput from "../buttonInput/ButtonInput";
+import TextInput from "../textInput/TextInput";
 
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
+  const [username, setUsername] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
-    const [newEmail, setNewEmail] = useState('');
-    const [newPhoneNumber, setNewPhoneNumber] = useState();
-    const [newPassword, setNewPassword] = useState('');
+  const navigate = useNavigate();
 
+  const backFunc = () => {
+    navigate(-1);
+  };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
 
+  const signupHandler = async () => {
+    try {
+      const responst = await fetch("http://localhost:8000/api/v1/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: username,
+          email: newEmail,
+          password: newPassword,
+        }),
+      });
 
-    const navigate = useNavigate();
-
-
-    const backFunc = () => {
-        navigate(-1);
+      const result = await responst.json();
+      if (result.token) {
+        navigate("/login");
+      }
+    } catch (error) {
+      throw new error(error);
     }
+  };
 
+  return (
+    <div className="signup-page-container">
+      <NavigationHeader
+        title="Sign up"
+        titleClassName="navigation-header-signup"
+        NavigationHeaderImage={backButtonImage}
+        NavigationHeaderImageClassName="back-button-image-full"
+        onClick={backFunc}
+      />
 
-    return (
-        <div className='signup-page-container'>
+      <div className="signup-form-container">
+        <form action="/signup" onSubmit={submitHandler} className="signup-form">
+          <TextInput
+            className="signup-user-email-input"
+            type="text"
+            labelTitle="Name"
+            placeholder="Enter your name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-            <NavigationHeader
-                title='Sign up'
-                titleClassName='navigation-header-signup'
-                NavigationHeaderImage={backButtonImage}
-                NavigationHeaderImageClassName='back-button-image-full'
-                onClick={backFunc}
-            />
+          <TextInput
+            className="signup-user-phone-number-input"
+            type="email"
+            labelTitle="Email"
+            placeholder="Enter your email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+          />
 
-            <div className='signup-form-container'>
+          <TextInput
+            className="signup-user-password-input"
+            type="password"
+            labelTitle="Password"
+            placeholder="Password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+          <ButtonInput
+            type="submit"
+            className="signup-submit-button-input"
+            title="Submit"
+            onClick={() => {
+              signupHandler();
+              // navigate("/cart")
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-                <form action="" className='signup-form'>
-                    <TextInput
-                        className='signup-user-email-input'
-                        type='email'
-                        labelTitle='Email'
-                        placeholder='Email'
-                        value={newEmail}
-                        onChange={(e) => setNewEmail(e.target.value)}
-                    />
-
-                    <TextInput
-                        className='signup-user-phone-number-input'
-                        type='number'
-                        labelTitle='Phone Number'
-                        placeholder='Phone Number'
-                        value={newPhoneNumber}
-                        onChange={(e) => setNewPhoneNumber(e.target.value)}
-                    />
-
-                    <TextInput
-                        className='signup-user-password-input'
-                        type='password'
-                        labelTitle='Password'
-                        placeholder='Password'
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                    />
-                    <ButtonInput
-                        type='submit'
-                        className='signup-submit-button-input'
-                        title='Submit'
-                        onClick={() => navigate('/cart')}
-                    />
-
-                </form>
-
-            </div>
-
-
-
-        </div>
-    )
-}
-
-export default SignupPage
+export default SignupPage;
