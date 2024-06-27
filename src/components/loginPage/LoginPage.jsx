b2fabf9099f87ceda09651220ea6a851731266f0
akshu2626 <akshugajbhiye97@gmail.com>
2024-06-27 14:02:43 +0530

    Ingerate Login page api

--- src/components/loginPage/LoginPage.jsx
+++ src/components/loginPage/LoginPage.jsx
@@ -1,75 +1,100 @@
-import { useState } from 'react';
-import './LoginPage.css';
-import NavigationHeader from '../navigationHeader/NavigationHeader';
-import ButtonInput from '../buttonInput/ButtonInput';
-import TextInput from '../textInput/TextInput';
-import backButtonImage from '../../image/BackButton.svg'
+import { useState } from "react";
+import "./LoginPage.css";
+import NavigationHeader from "../navigationHeader/NavigationHeader";
+import ButtonInput from "../buttonInput/ButtonInput";
+import TextInput from "../textInput/TextInput";
+import backButtonImage from "../../image/BackButton.svg";
 
-
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const [emailOrPhone, setEmailOrPhone] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [emailOrPhone, setEmailOrPhone] = useState('');
-    const [password, setPassword] = useState('');
+  const htmlErrow = ">";
 
-    const htmlErrow = '>'
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const loginUserFormFunc = (event) => {
+    event.preventDefault();
+  };
 
-    const loginUserFormFunc = (event) => {
-        event.preventDefault();
-    };
+  const backFunc = () => {
+    navigate(-1);
+  };
+  const logInHandler = async () => {
+    try {
+      const responst = await fetch(
+        `${process.env.REACT_APP_SIGNUP_URL}/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: emailOrPhone,
+            password: password,
+          }),
+        }
+      );
 
+      const result = await responst.json();
+      if (result.token) {
+        navigate("/cart");
+      }
+    } catch (error) {}
+  };
 
-    const backFunc = () => {
-        navigate(-1);
-    }
+  return (
+    <div className="login-page-container">
+      <NavigationHeader
+        title="Log in"
+        titleClassName="navigation-header-login"
+        NavigationHeaderImage={backButtonImage}
+        NavigationHeaderImageClassName="back-button-image-full"
+        onClick={backFunc}
+      />
 
-    return (
-        <div className='login-page-container'>
-            <NavigationHeader 
-            title='Log in'
-            titleClassName='navigation-header-login'
-            NavigationHeaderImage={backButtonImage}
-            NavigationHeaderImageClassName='back-button-image-full'
-            onClick={backFunc}
-            />
-            
-            <div className='login-form-container' >
+      <div className="login-form-container">
+        <form className="login-form" onSubmit={loginUserFormFunc} action="">
+          <TextInput
+            className="login-user-id-input"
+            type="text"
+            labelTitle="Email"
+            placeholder="Email"
+            value={emailOrPhone}
+            onChange={(e) => setEmailOrPhone(e.target.value)}
+          />
 
-                <form className='login-form' onSubmit={loginUserFormFunc} action="">
-                    <TextInput
-                        className='login-user-id-input'
-                        type='text'
-                        labelTitle='Email or Phone Number'
-                        placeholder='Email or Phone Number'
-                        value={emailOrPhone}
-                        onChange={(e) => setEmailOrPhone(e.target.value)}
-                    />
+          <TextInput
+            className="login-user-password-input"
+            type="password"
+            labelTitle="Password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                    <TextInput
-                        className='login-user-password-input'
-                        type='password'
-                        labelTitle='Password'
-                        placeholder='Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-
-                    <ButtonInput
-                        type='submit'
-                        className='login-submit-button-input'
-                        title='Submit'
-                        onClick={() => navigate('/cart')}
-                    />
-                </form>
-                <div className='login-page-information-container'>
-                    <p className='login-page-information'><span>Use the cash register code that can be created by the owner in</span><span>Manage Store -{htmlErrow} Cashier Code</span></p>
-                </div>
-            </div>
+          <ButtonInput
+            type="submit"
+            className="login-submit-button-input"
+            title="Submit"
+            onClick={() => {
+              logInHandler();
+            }}
+          />
+        </form>
+        <div className="login-page-information-container">
+          <p className="login-page-information">
+            <span>
+              Use the cash register code that can be created by the owner in
+            </span>
+            <span>Manage Store -{htmlErrow} Cashier Code</span>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
