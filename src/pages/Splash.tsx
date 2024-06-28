d0a401ccc5282abee1fefe7c3b171ad5443fa929
ypriyam <ritikshilpkar23@gmail.com>
2024-06-28 16:18:26 +0530

    add check for login user

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SplashScreen from '../components/splashScreen/Splashscreen'
+import { useAppContext } from '../appState/appStateContext';
+import { useNavigate } from 'react-router-dom';
 
 const Splash = () => {
+
+  const { globalState } = useAppContext();
+  const navigate = useNavigate();
+  useEffect(() => {
+  if (globalState?.isLoggedIn) {
+     navigate("/cart")
+  }
+  // eslint-disable-next-line
+  }, [globalState?.isLoggedIn]);
   return (
     <SplashScreen/>
   )
