7380791a2a4ed01fb4905f36f726208645686f62
nitinmasih <nitin.masih02@gmail.com>
2024-07-08 18:01:52 +0530

    change in menu page

--- src/components/createMenuPage/MenuPageBody.jsx
+++ src/components/createMenuPage/MenuPageBody.jsx
@@ -102,7 +102,6 @@ const MenuPageBody = () => {
               position: "absolute",
               top: "50%",
               left: "50%",
-
               transform: "translate(-50%, -50%)",
             }}
           />
--- src/icons/MinusIcon.jsx
+++ src/icons/MinusIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MinusIcon = ({onclick,className}) => {
   return (
-    <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus" onClick={onclick}><path d="M5 12h14"/></svg>
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" width="22" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus" onClick={onclick}><path d="M5 12h14"/></svg>
   )
 }
 
